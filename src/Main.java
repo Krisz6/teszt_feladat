31faa6dcdfcbbf8147f905f83e7fc1bc55bedaba
Krisz6 <szalay.krisztian93@gmail.com>
2023-08-31 17:46:50 +0200

    min and even number

--- src/Main.java
+++ src/Main.java
@@ -14,4 +14,22 @@ public class Main {
             System.out.print(array[i] + " ");
         }
     }
+    public static int findMin(int[] array){
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]<min){
+                min=array[i];
+            }
+        }
+        return min;
+    }
+    public static int countEvenNumber(int[] array){
+        int count = 0;
+        for (int actual : array){
+            if (actual % 2 == 0){
+                count++;
+            }
+        }
+        return count;
+    }
 }
